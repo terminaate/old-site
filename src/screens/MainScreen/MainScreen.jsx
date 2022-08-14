5fd402c6446c9aeb8604bc2a0c868b258b3b48a1
terminaate <tntwnik@gmail.com>
2022-08-14 23:03:41 +0300

    Remove notification from main screen

--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -1,30 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cl from './MainScreen.module.css';
-import AnimatedSymbolsText from '@/components/AnimatedSymbolsText/AnimatedSymbolsText'
+import AnimatedSymbolsText from '@/components/AnimatedSymbolsText/AnimatedSymbolsText';
 import Particles from '@/components/Particles/Particles';
 import Header from '@/components/Header/Header';
-import Notification from '../../components/Notification/Notification';
 
 const MainScreen = () => {
-	const [notify, setNotify] = useState("");
 
 	return (
 		<>
 			<Header />
-			<div onClick={() => setNotify("HELLO WORLD")} className={cl.mainScreen}>
+			<div className={cl.mainScreen}>
 				<Particles particlesCount={20} height={120} className={cl.logoParticles} />
 				<AnimatedSymbolsText className={cl.logo} infinite={true}>
 					Terminaate
 				</AnimatedSymbolsText>
-				{/*<div className={cl.socials}>*/}
-				{/*	<button onClick={() => open('//github.com/terminaate')}>*/}
-				{/*		<img src={githubImg} alt='' />*/}
-				{/*	</button>*/}
-				{/*	<button>*/}
-				{/*		<img src={discordImg} alt='' />*/}
-				{/*	</button>*/}
-				{/*</div>*/}
-				<Notification state={notify} setState={setNotify}/>
 			</div>
 		</>
 	);
