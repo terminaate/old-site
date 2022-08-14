02a8d32b54a4960115f724414c659a32e69f193d
terminaate <tntwnik@gmail.com>
2022-08-14 12:12:48 +0300

    First version of github modal

--- src/components/GithubModal/GithubModal.jsx
+++ src/components/GithubModal/GithubModal.jsx
@@ -5,7 +5,15 @@ import GithubService from '@/services/GithubService';
 import usersImg from '!/users.png';
 import locationImg from '!/location.png';
 import linkImg from '!/link.png';
+import clockImg from '!/clock.svg';
+import htmlImg from '!/html.svg';
+import cssImg from '!/css.svg';
+import javascriptImg from '!/javascript.svg';
+import typescriptImg from '!/typescript.svg';
+import pythonImg from '!/python.svg';
 import classes from '@/hooks/classes';
+import AnimatedSymbolsText from '../AnimatedSymbolsText/AnimatedSymbolsText';
+
 
 const GithubModal = ({ modal, setModal }) => {
 	const [user, setUser] = useState({});
@@ -15,21 +23,37 @@ const GithubModal = ({ modal, setModal }) => {
 		promise.then(r => r.status === 200 ? setState(r.body) : '');
 	};
 
+	const getLanguageIcon = (lang) => {
+		const langs = {
+			'JavaScript': javascriptImg,
+			'Python': pythonImg,
+			'HTML': htmlImg,
+			'CSS': cssImg,
+			'TypeScript': typescriptImg
+		};
+		return langs[lang];
+	};
+
 	useEffect(() => {
 		setAsyncState(setUser, GithubService.getMe());
 		setAsyncState(setRepos, GithubService.getMyRepos());
 	}, []);
 
+	const getDateAndMonthName = (date) => {
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		return `${date.getDate()} ${months[date.getMonth()]}`;
+	};
+
 	return (
 		<>
 			{Object.values(user).length > 0 && (
-				<MovableModal minWidth={"800px"} className={cl.githubModal} width={'50%'} title={'My github ❤'}
+				<MovableModal minWidth={'800px'} className={cl.githubModal} width={'50%'} title={'My github ❤'}
 											height={'60%'} modal={modal}
 											setModal={setModal}>
 					<div className={cl.userInfo}>
 						<div className={cl.userAvatar} style={{ backgroundImage: `url(${user.avatar_url})` }} />
 						<span className={cl.userName}>{user.name}</span>
-						<span className={cl.userNickname}>{user.login}</span>
+						<AnimatedSymbolsText infinite={true} className={cl.userNickname}>{user.login}</AnimatedSymbolsText>
 						<span className={cl.userBio}>{user.bio}</span>
 						<div className={cl.userFollowers}>
 							<img src={usersImg} alt='' />
@@ -39,13 +63,29 @@ const GithubModal = ({ modal, setModal }) => {
 							<img src={locationImg} alt='' />
 							<span>{user.location}</span>
 						</div>
-						<div className={classes(cl.userFollowers, cl.userLocation)}>
+						<div onClick={() => setUser({ ...user, blog: 'You already here ;)' })}
+								 className={classes(cl.userFollowers, cl.userLocation)}>
 							<img src={linkImg} alt='' />
 							<span>{user.blog}</span>
 						</div>
 					</div>
 					<div className={cl.userRepos}>
-						Hello world 2
+						{repos.map((repo, index) => (
+							<div className={cl.repoContainer} key={index}>
+								<div className={cl.repoHeader}>
+									<div className={cl.repoName} onClick={() => open(repo.html_for, '_blank')}>
+										<img src={repo.owner.avatar_url} alt='' />
+										<span>{repo.full_name}</span>
+									</div>
+									<img className={cl.repoLanguage} src={getLanguageIcon(repo.language)} alt='' />
+								</div>
+								<div className={cl.repoDesc}>{repo.description}</div>
+								<div className={cl.repoLastCommit}>
+									<img src={clockImg} alt='' />
+									<span>{getDateAndMonthName(new Date(repo.updated_at))} - last commit</span>
+								</div>
+							</div>
+						))}
 					</div>
 				</MovableModal>
 			)}
